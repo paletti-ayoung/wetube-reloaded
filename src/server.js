fd5138266bb1ec47de3f353b7931f32b0d969acb
ayoung <ayoung.jang@paletti.ai>
2021-06-03 23:48:44 +0900

    #4.8

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 // app.use() = can create global middleware, always use -> get, left
 import express from "express";
 import morgan from "morgan";
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const PORT = 4000;
 
@@ -10,30 +13,9 @@ const logger = morgan("dev");
 
 app.use(logger);
 
-const globalRouter= express.Router();
-
-const handleHome = (req,res) => res.send("Home");
-
-globalRouter.get("/",handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req,res)=>res.send("Edit User");
-
-userRouter.get("/edit",handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req,res) => res.send("Watch Video");
-
-videoRouter.get("/watch",handleWatchVideo);
-
 app.use("/",globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users",userRouter);
-
-
-
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🌈`);
 
