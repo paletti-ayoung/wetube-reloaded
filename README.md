e107267b90e2e7361acfa2a1699d5f59d6217be9
ayoung <ayoung.jang@paletti.ai>
2021-06-07 15:15:58 +0900

    #5.0

---
+++ README.md
@@ -0,0 +1,19 @@
+#Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/join
+users/login
+
+/users/:id -> See User
+/users/logout -> Log Out
+/usres/edit -> Edit MY Profile
+/users/delete -> Delete My Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
--- src/server.js
+++ src/server.js
@@ -5,21 +5,17 @@ import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 
-const PORT = 4000;
 
 const app = express(); // create application
 
 const logger = morgan("dev");
 
-
 app.set("view engine", "pug"); //set the engine to pug.
 app.set("views",process.cwd()+"/src/views");
 app.use(logger);
-
+app.use(express.urlencoded({extended:true})); // form understand
 app.use("/",globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users",userRouter);
 
-const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🌈`);
-
-app.listen(PORT, handleListening);
+export default app;
