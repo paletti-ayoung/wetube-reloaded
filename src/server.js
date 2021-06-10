37b2309b6e59923f43fb458a587defef64b05e8b
ayoung <ayoung.jang@paletti.ai>
2021-06-10 23:45:33 +0900

    #9.7

--- src/server.js
+++ src/server.js
@@ -27,15 +27,13 @@ app.use(
     secret:process.env.COOKIE_SECRET,
     resave:false,
     saveUninitialized:false,
-    // cookie:{
-    //     maxAge:20000,
-    // },
     store:MongoStore.create({mongoUrl:process.env.DB_URL}),
 
 }));
 
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/",rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users",userRouter);
