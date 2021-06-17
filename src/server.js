47560b23b72727aa38996fc59a12c4ff59136658
ayoung <ayoung.jang@paletti.ai>
2021-06-17 17:18:38 +0900

    #15.1

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import morgan from "morgan";
 import session from "express-session";
 import MongoStore from "connect-mongo";
+import flash from 'express-flash';
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
@@ -32,6 +33,7 @@ app.use(
 
 }));
 
+app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
